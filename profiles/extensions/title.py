6868012f45a2dc2c3c570bbcad981acb460f57ed
Max Peterson <max@incuna.com>
2011-01-27 16:21:48 +0000

    Tweaked title extension

--- profiles/extensions/title.py
+++ profiles/extensions/title.py
@@ -1,16 +1,16 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-TITLE_CHOICES = [('Dr','Dr'),
-                 ('Professor','Professor'),
-                 ('Mr','Mr'),
-                 ('Miss','Miss'),
-                 ('Mrs','Mrs'),
-                 ('Ms','Ms'),
-                ]
 
 def register(cls, admin_cls):
-    cls.add_to_class('title', models.CharField(max_length=255, verbose_name=_('title'), null=True, blank=True, choices=TITLE_CHOICES))
+    cls.add_to_class('TITLE_CHOICES', [('Dr','Dr'),
+                                       ('Professor','Professor'),
+                                       ('Mr','Mr'),
+                                       ('Miss','Miss'),
+                                       ('Mrs','Mrs'),
+                                       ('Ms','Ms'),
+                                      ])
+    cls.add_to_class('title', models.CharField(max_length=255, verbose_name=_('title'), null=True, blank=True, choices=cls.TITLE_CHOICES))
 
     from profiles.models import Profile
     get_full_name = Profile.get_full_name
