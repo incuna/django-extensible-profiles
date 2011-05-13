a2d66cb33dcf3eff6027d9c20624188c50b54b6b
Katia ekaterina@incuna.com <Katia ekaterina@incuna.com>
2011-05-13 15:40:37 +0100

    Renamed telephone label

--- profiles/extensions/address.py
+++ profiles/extensions/address.py
@@ -8,7 +8,7 @@ def register(cls, admin_cls):
     cls.add_to_class('region', models.CharField(max_length=255, verbose_name=_('county/state/province'), null=True, blank=True))
     cls.add_to_class('postcode', models.CharField(max_length=15, verbose_name=_('postcode'), null=True, blank=True))
     cls.add_to_class('country', models.ForeignKey('countries.Country', null=True, blank=True))
-    cls.add_to_class('telephone', models.CharField(max_length=32, verbose_name=_('mobile number'), null=True, blank=True))
+    cls.add_to_class('telephone', models.CharField(max_length=32, verbose_name=_('telephone'), null=True, blank=True))
 
     if admin_cls:
         admin_cls.search_fields += ['address1', 'address2', 'city', 'region', 'postcode']
