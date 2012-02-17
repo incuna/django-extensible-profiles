a14ca1588473499ab6789007427ecdf7d3ac4615
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-02-17 09:57:34 +0000

    Add primary field on address.
    Also it really didn't need a fk as it's being used as a M2M.

--- profiles/modules/addresses/models.py
+++ profiles/modules/addresses/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Address(models.Model):
-    profile = models.ForeignKey('profiles.Profile')
+    primary = models.BooleanField(default=False)
     address1 = models.CharField(max_length=255, verbose_name=_('address'), null=True, blank=True)
     address2 = models.CharField(max_length=255, verbose_name=_('address 2'), null=True, blank=True)
     city = models.CharField(max_length=255, verbose_name=_('town/city'), null=True, blank=True)
