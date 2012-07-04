4b6e92107ea3a1e0d425b22c9b5835996203452c
mlen108 <matt.lenc@gmail.com>
2012-07-04 15:54:40 +0100

    Add RegisterView which allows to create new user profile (issue #10).

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,15 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 
-from profiles.views import ProfileEdit, ProfileView
+from profiles.views import ProfileEdit, ProfileView, RegisterView
 
 
 urlpatterns = patterns('profiles.views',
-    url(r'^$', ProfileView.as_view(), name='profile'),
-    url(r'^edit/$', ProfileEdit.as_view(), name='profile-edit'),
+    url(r'^profile/$', ProfileView.as_view(), name='profile'),
+    url(r'^profile/edit/$', ProfileEdit.as_view(), name='profile-edit'),
 )
 
+if getattr(settings, 'REGISTRATION_ON_VIEW', False):
+    urlpatterns += patterns('profiles.views',
+        url(r'^profile/register/$', RegisterView.as_view(), name='profile-register'),
+    )
--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from django.contrib import messages
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse, get_callable
-from django.views.generic import TemplateView, UpdateView
+from django.views.generic import TemplateView, CreateView, UpdateView
+
+from incuna.utils.unique_id import generate_id
 
 from profiles.models import Profile
 from profiles.utils import class_view_decorator
@@ -13,10 +16,52 @@ try:
 except AttributeError:
     from forms import ProfileForm
 
+try:
+    RegistrationForm = get_callable(settings.REGISTRATION_FORM_CLASS)
+except AttributeError:
+    RegistrationForm = ProfileForm
+
 
 @class_view_decorator(login_required)
 class ProfileView(TemplateView):
     template_name = 'profiles/profile.html'
+
+
+class RegisterView(CreateView):
+    form_class = RegistrationForm
+    template_name = 'profiles/profile_form.html'
+    success_url = getattr(settings, 'REGISTRATION_COMPLETE_URL',
+        settings.LOGIN_REDIRECT_URL)
+
+    def form_valid(self, form):
+        if not 'username' in form.cleaned_data:
+            # auto generate a username
+            generate_kwargs = {}
+            for fname in ['first_name', 'last_name', 'email']:
+                if fname in form.cleaned_data:
+                    generate_kwargs[fname] = form.cleaned_data[fname]
+            form.cleaned_data['username'] = generate_id(**generate_kwargs)
+
+        # try to get the password
+        password = None
+        for fname in ['password', 'password1', 'password2', 'plain_password']:
+            if fname in form.cleaned_data:
+                password = form.cleaned_data[fname]
+                break
+
+        obj = form.save(commit=False)
+
+        if password:
+            obj.set_password(password)
+
+        obj.save()
+
+        if password:
+            user = authenticate(username=form.cleaned_data['username'], password=password)
+            messages.info(self.request, 'Your profile has been created.')
+            login(self.request, user)
+
+        return super(RegisterView, self).form_valid(form)
 
 
 @class_view_decorator(login_required)
