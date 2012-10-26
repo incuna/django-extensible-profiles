1938a6fdb28c6477c7ee3465c613a036791c8254
mlen108 <matt.lenc@gmail.com>
2012-10-26 09:18:30 +0100

    Regexs are baaad

--- profiles/modules/codes/fields.py
+++ profiles/modules/codes/fields.py
@@ -7,14 +7,12 @@ from .models import Code
 
 
 class CodeField(fields.CharField):
-    re_clean_code = re.compile('[^a-zA-Z0-9]+')
-
     def to_python(self, value):
         if not value:
             return value
 
         try:
-            value = self.re_clean_code.sub('', value)
+            value = value.strip()
             value = Code.objects.get(code__iexact=value, is_active=True)
         except Code.DoesNotExist:
             raise exceptions.ValidationError('Enter a valid registration code.')
